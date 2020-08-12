a660834a2ed1e63e4c1d54c37e9a91696fb275fe
chrislyr0420 <li.yiran@wustl.edu>
2020-08-12 01:29:03 -0700

    Create Bottom bar
    Create bottom_nav_menu.xml and use LinearLayout to create the ui

--- app/src/main/java/com/laioffer/tinnews2/TinNewsMainActivity.java
+++ app/src/main/java/com/laioffer/tinnews2/TinNewsMainActivity.java
@@ -1,10 +1,6 @@
 package com.laioffer.tinnews2;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,18 +11,5 @@ public class TinNewsMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tin_news_main);
 
-        final TextView welcomeTextView = findViewById(R.id.welcomeTextView);
-        final EditText nameEditText = findViewById(R.id.nameEditText);
-        final EditText emailEditText = findViewById(R.id.emailEditText);
-        Button submitButton = findViewById(R.id.submitButton);
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = nameEditText.getText().toString();
-                String email = emailEditText.getText().toString();
-                welcomeTextView.setText("Name: " + name + ", \nEmail address: " + email);
-            }
-        });
     }
 }
