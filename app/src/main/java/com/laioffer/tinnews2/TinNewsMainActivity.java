350407297bf77448f3498ddbf91f3eb055cb187d
chrislyr0420 <li.yiran@wustl.edu>
2020-08-13 00:35:25 -0700

    Learn how to handle navigation using Navigation component
    Notice this should be a new feature with AndroidX, which is also new from former project
    
    This implementation involves:
    * Create four fragments, one for each nav component
    * Create a navigation resource named nav_graph and create fragments view
    * Edit main view.xml to let the FragmentContainerView implement navGraph
    * Edit MainActivity.java to combine everything together.
     * Create the BottomNavigationView
     * Create a NavHostFragment
     * Create the NavController and setup controller into the activity

--- app/src/main/java/com/laioffer/tinnews2/TinNewsMainActivity.java
+++ app/src/main/java/com/laioffer/tinnews2/TinNewsMainActivity.java
@@ -2,14 +2,33 @@ package com.laioffer.tinnews2;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 public class TinNewsMainActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tin_news_main);
 
+        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        assert navHostFragment != null;
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
     }
 }
