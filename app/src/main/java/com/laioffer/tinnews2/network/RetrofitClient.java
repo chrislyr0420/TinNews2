2d494d4b28e53668caf5852286bd387f5ddbc9ba
chrislyr0420 <li.yiran@wustl.edu>
2020-09-03 01:26:40 -0700

    Enable Stetho debugging tool and test network requests through Chrome: chrome://inspect:

--- app/src/main/java/com/laioffer/tinnews2/network/RetrofitClient.java
+++ app/src/main/java/com/laioffer/tinnews2/network/RetrofitClient.java
@@ -3,6 +3,7 @@ package com.laioffer.tinnews2.network;
 import android.content.Context;
 
 import com.ashokvarma.gander.GanderInterceptor;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import java.io.IOException;
 
@@ -26,6 +27,7 @@ public class RetrofitClient {
         OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
                 .addInterceptor(new HeaderInterceptor())
                 .addInterceptor(new GanderInterceptor(context).showNotification(true))
+                .addNetworkInterceptor(new StethoInterceptor())
                 .build();
 
         return new Retrofit.Builder()
